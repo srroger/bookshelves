9339a67f3485834790ebf254cb4903df3abbe390
Roger Srey <rsrey@fullmetal.central.com>
2018-11-20 10:17:39 +0100

    Added a connection and deconnection system

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import {AuthService} from '../services/auth.service';
+
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+	isAuth : boolean;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  	firebase.auth().onAuthStateChanged(
+  		(user) => {
+  			if(user) {
+  				this.isAuth = true;
+  			} else  {
+  				this.isAuth = false;
+  			}
+ 			}
+  		);
+  }
+
+  onSignOut() {
+  	this.authService.signOutUser();
   }
 
 }
